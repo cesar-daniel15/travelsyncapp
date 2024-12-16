09fb7ec7993fb1a48a4f3200a2b214f21eec0bb5
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-12-16 14:38:37 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'travelsyncapp',
+  webDir: 'www'
+};
+
+export default config;
