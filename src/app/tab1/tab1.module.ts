463c4385b0bb29404598e3bbabb21d7c912e5a4f
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2025-01-09 18:44:27 +0000

    Update tab1.module.ts

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -8,6 +8,7 @@ import { Tab1Page } from './tab1.page';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { TranslateModule } from '@ngx-translate/core';
 
+
 @NgModule({
   imports: [
     IonicModule,
