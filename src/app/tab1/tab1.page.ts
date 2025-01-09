7cf63add29c08836feb278bc9f057adf81f64d17
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2025-01-09 18:45:36 +0000

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -47,8 +47,8 @@ interface Travels{
 export class Tab1Page implements OnInit {
 
   apiUrl: string = "https://mobile-api-one.vercel.app/api";
-  name: string = "#";
-  password: string = "#";
+  name: string = "cesar.daniel@ipvc.pt";
+  password: string = "uVt(D!u3";
 
   selectedState: State = State.Planed;
   selectedType: Type = Type.Business;
